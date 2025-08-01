430b6b1dc395dcf3ddb8c20abec19b42bd26d48f
Ravikant Singh Rajput <122196249+raavikant27@users.noreply.github.com>
2025-08-01 10:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Non-leap year
+  }
+}
